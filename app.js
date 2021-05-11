db059534cb5b7873389075f5b4f04991178c8aef
Горохов Павел Олегович <POlGorokhov@sberbank.ru>
2021-05-11 04:28:13 +0300

    add coments

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ import {setRun} from './src/Modules/Commission/Controllers'
 
 const inputFileName = process.argv[2]
 
+/**
+ * Use function from Comission module
+ */
 setRun(inputFileName)
--- src/Modules/Commission/Controllers/index.ts
+++ src/Modules/Commission/Controllers/index.ts
@@ -1,22 +1,36 @@
-import fs from 'fs'
-import util from 'util'
+const fs = require('fs')
+const util = require('util')
+
 import {encoding} from '../../../Common/const'
 import {ITransaction} from '../models'
 
-const readFile = util.promisify(fs.readFile)
-
+/**
+ * Read file thought encoding
+ * @param inputFileName name of current file in current directory
+ */
 const getStuff = (inputFileName: string) => {
+    const readFile = util.promisify(fs.readFile)
+
     return readFile(inputFileName, encoding)
 }
 
-const getListFromJson = (json_data: string): ITransaction[] => {
+/**
+ * Parse input file to array of current type
+ * @param jsonData input json string
+ */
+const getListFromJson = (jsonData: string): ITransaction[] => {
     const regex = /\,(?=\s*?[\}\]])/g
-    const correct = json_data.replace(regex, '')
+    const correct = jsonData.replace(regex, '')
 
     return JSON.parse(correct)
 }
 
-export const setRun = (inputFile: string) => {
+export const setRun = (inputFile?: string) => {
+    if (!inputFile) {
+        console.log('No input file')
+        return
+    }
+
     getStuff(inputFile).then((data) => {
         console.log(getListFromJson(data))
     })
