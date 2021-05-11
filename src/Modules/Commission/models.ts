fbaef9ea1d6165a0e5083e050997dec6fb789d97
Горохов Павел Олегович <POlGorokhov@sberbank.ru>
2021-05-11 05:02:22 +0300

    ref in types

--- src/Modules/Commission/models.ts
+++ src/Modules/Commission/models.ts
@@ -1,12 +1,46 @@
+/**
+ * About date and user that 've done current transaction
+ */
 export interface ITransaction {
     date: string
     user_id: number
-    user_type: string
-    type: string
+    user_type: EUserType
+    type: EOperationType
     operation: ICurrOperation
 }
 
+/**
+ * About amount and currency
+ */
 export interface ICurrOperation {
     amount: number
-    currency: string
+    currency: ECurrency
+}
+
+/**
+ * Types of user
+ * @param NATURAL natural person
+ * @param JURIDICAL legal person
+ */
+export enum EUserType {
+    NATURAL = 'natural',
+    JURIDICAL = 'juridical',
+}
+
+/**
+ * Types of operations
+ * @param CASH_IN
+ * @param CASH_OUT
+ */
+export enum EOperationType {
+    CASH_IN = 'cash_in',
+    CASH_OUT = 'cash_out',
+}
+
+/**
+ * Types of currency
+ * @param EUR
+ */
+export enum ECurrency {
+    EUR = 'EUR',
 }
