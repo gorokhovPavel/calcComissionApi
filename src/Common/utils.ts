d9cc1aebfb94adbc63497a569c8b2127adbeff0b
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-07-02 00:54:48 +0300

    Update utils.ts

--- src/Common/utils.ts
+++ src/Common/utils.ts
@@ -4,7 +4,7 @@ const util = require('util')
 import {encoding} from './const'
 
 /**
- * Mode of reading file through encoding
+ * Mode of reading file encoding
  * @param inputFileName name of current file in current directory
  */
 export const getReadFile = (inputFileName: string) => {
